11ef2ce532811bb717a0c0b1c7a17ee0c5c1e270
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-12-07 13:20:35 +0000

    Update broken.js

--- scripts/commands/broken.js
+++ scripts/commands/broken.js
@@ -21,7 +21,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["-🔰 -♦𝗕Ø𝗦𝗦 𝗧𝗔𝗡𝗩𝗜𝗥♦- 🔰-"];
+   var hi = ["●❯────────────────❮●\n         -♦𝐓𝐀𝐍𝐕𝐈𝐑-𝐁𝐎𝐓♦-         \n●❯────────────────❮●"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
