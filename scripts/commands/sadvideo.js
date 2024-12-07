0b591f6fac11457dd16bc904811bca734d92c44b
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-12-07 13:27:42 +0000

    Update sadvideo.js

--- scripts/commands/sadvideo.js
+++ scripts/commands/sadvideo.js
@@ -26,7 +26,7 @@ module.exports.run = async ({ api, event, args }) => {
         response.data.pipe(writer);
 
         writer.on('finish', () => {
-            api.sendMessage({ body: "●❯────────────────❮●\n        -♦𝗕Ø𝗦𝗦 𝗧𝗔𝗡𝗩𝗜𝗥♦-       \n●❯────────────────❮●", attachment: fs.createReadStream(videoPath) }, event.threadID);
+            api.sendMessage({ body: "●❯────────────────❮●\n         -♦𝐓𝐀𝐍𝐕𝐈𝐑-𝐁𝐎𝐓♦-         \n●❯────────────────❮●", attachment: fs.createReadStream(videoPath) }, event.threadID);
         });
 
         writer.on('error', (err) => {
